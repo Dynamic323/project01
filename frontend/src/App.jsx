d73532ffa0eba1b232fbdcacd773d554980f9835
DY <otamereisrael8@gmail.com>
2025-07-09 10:20:48 +0100

    created login and register page Auth next.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Content from "./Components/Content";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setemail] = useState("");
-  const [phone, setphone] = useState("");
-  // const sendToWhatsApp = (event) => {
-
-  // };
   return (
-    <div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-
-          if (name == "" || email == "") {
-            alert("error can not be empty");
-          }
-
-          const msg = `
-    Hello my name is ${name}
-    `;
-
-          console.log(name, email, phone);
-        }}
-      >
-        <fieldset>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            placeholder="Your name"
-            required
-          />
-          <br />
-          <input
-            type="email"
-            id="email"
-            onChange={(e) => setemail(e.target.value)}
-            value={email}
-            placeholder="Your email"
-            required
-          />
-          <br />
-          <input
-            type="number"
-            onChange={(e) => setphone(e.target.value)}
-            value={phone}
-            placeholder="+234"
-          />
-          <button type="submit">Send via WhatsApp</button>
-        </fieldset>
-      </form>
+    <div className=" ">
+      {/* <Navbar /> */}
+      <Content />
     </div>
   );
 }
