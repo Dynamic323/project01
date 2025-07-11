2c066f00ec6d9498d2acab15e6ddd515fa829ddf
DY <otamereisrael8@gmail.com>
2025-07-11 03:06:22 +0100

    Working on the Registration form now

--- frontend/src/Guest/Pages/Form/Register.jsx
+++ frontend/src/Guest/Pages/Form/Register.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../../Components/Navbar";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const [formFildes, setFormfields] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  //   const handelChange = (e)=>
+  //   {
+  //     const {name , value} = e.target
+  //     setFormfields((prev)=>({
+  //         ...prev,
+  //         {
+  //             [name]:value
+  //         }
+  //     }))
+  //   }
+
+  function handelChange(e) {
+    const { name, value } = e.target;
+
+    setFormfields((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  const HandelSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <div className="mt-auto ">
+      <div className="mt-auto " onSubmit={HandelSubmit}>
         <div className=" max-w-md mx-auto ">
-          <form action=" ">
+          <form action=" " onSubmit={HandelSubmit}>
             <div className=" text-3xl "> create an Account </div>
 
             <div className="flex flex-col my-5 gap-7">
@@ -15,30 +45,47 @@ function Register() {
                 className="border border-slate-400 p-3 rounded-s-2xl"
                 type="text"
                 placeholder="User Name"
+                name="name"
+                onChange={handelChange}
+                value={formFildes.name}
               />
+
               <input
                 className="border border-slate-400 p-3 rounded-s-2xl"
                 type="email"
+                name="email"
                 placeholder="Email"
+                onChange={handelChange}
+                value={formFildes.email}
               />
               <input
                 className="border border-slate-400 p-3 rounded-s-2xl"
                 type="password"
                 placeholder="Password"
+                name="Password"
+                value={formFildes.password}
+                onChange={handelChange}
               />
+            </div>
+
+            <span> or sign in with </span>
+
+            <div className="">
+              <button>
+                <i class="fa-brands fa-google"> </i>
+              </button>
+              <button></button>
             </div>
 
             <button
               type="submit"
               className="w-full mb-3  py-3 rounded-2xl bg-orange-600"
             >
-              {" "}
               Create
             </button>
             <span className="text-slate-400  ">
-              Already have an Account ?{" "}
+              Already have an Account ?
               <Link to="/login">
-                {" "}
                 <span>
                   <i className="text-orange-500 underline">Login in now</i>
                 </span>
--- frontend/src/context/Authcontext.jsx
+++ frontend/src/context/Authcontext.jsx
@@ -1,14 +1,64 @@
-import {  createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-import {onAuthStateChanged} from "firebase/auth"
-const AuthContext = createContext()
+import {
+  createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase/config";
+const AuthContext = createContext();
 
-export const AuthProvider = ({children)=>{
+export const AuthProvider = (children) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [user, setUser] = useState(null)
-    const [loading , setLoading] = useState(true)
+  useEffect(() => {
+    const unsuscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
 
-    useEffect(()=>{
-        const unsuscribe = onAuthStateChanged(auth)
-    })
-}
+    return unsuscribe();
+  }, []);
+
+  const register = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const login = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => signOut(auth);
+
+  const googleSignin = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  const githubSignIn = () => {
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, login, register, logout, googleSignin, githubSignIn }}
+    >
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
